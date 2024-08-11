21197c7e7025fcf941b26668a777c23a1d735322
goll <joaog.bastos@protonmail.ch>
2024-08-10 21:30:21 -0300

    Change default server config

--- src/config.ts
+++ src/config.ts
@@ -21,8 +21,8 @@ export type Config = {
 const server = {
     source: "site",
     root: "dist",
-    host: "::",
-    port: 8080,
+    host: "::1",
+    port: 48080,
     hash: {
         comments: "adff5dd6c568691636650f6c476e67aeab67c04071629ec758e80075f72bb375",
         blank: "5683b4ba31466c5999c35921ac36e39a968f75641fd8a1358389be50a3efc309",
