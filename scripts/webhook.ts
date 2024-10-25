56aaeda90976a8c55c7253d6fd9289d2a6c03f2c
goll <joaog.bastos@protonmail.ch>
2024-10-25 16:01:01 -0300

    Fix build scripts (and test webhook)

--- scripts/webhook.ts
+++ scripts/webhook.ts
@@ -11,35 +11,57 @@ const webhooks = new Webhooks({
     secret: process.env.WEBHOOK_SECRET!,
 });
 
+function getRequestBody(request: IncomingMessage): Promise<string> {
+    return new Promise(resolve => {
+        const chunks = [];
+
+        request.addListener("data", chunk => chunks.push(chunk))
+               .addListener("end", () => {
+                   const body = Buffer.concat(chunks).toString();
+                   resolve(body);
+               });
+    });
+}
+
+function spawnWithExitCode(command: string, args: string[]): Promise<number> {
+    return new Promise(resolve => {
+        const task = spawn(command, args, { stdio: "inherit" });
+        task.addListener("exit", code => resolve(code));
+    });
+}
+
 const handleWebhook = async (request: IncomingMessage, response: ServerResponse) => {
     const signature = request.headers["x-hub-signature-256"];
-    const body = request.read();
+    const body = await getRequestBody(request);
 
     const status = await webhooks.verify(body, signature as string);
 
     if (!status) {
+        console.warn("[webhook] received bogus event, ignoring...");
+
         response.statusCode = 401;
+        response.end();
+        
         return;
     }
 
+    console.log("[webhook] received push event from github repo");
+
     response.statusCode = 200;
+    response.end();
 
-    const git = spawn("git", ["pull"]);
+    const pull = await spawnWithExitCode("git", ["pull"]);
 
-    git.addListener("exit", async code => {
-        if (code !== 0)
-            throw new Error("`git pull' failed!");
+    if (pull !== 0)
+        console.warn("[webhook] `git pull' failed!");
 
-        await build("production");
+    await build("production");
 
-        // XXX: this is duplicated in src/config.ts  
-        const rsync = spawn("rsync", ["-r", "-p", "dist", process.env.SERVER_ROOT]);
+    // XXX: this is duplicated in src/config.ts  
+    const rsync = await spawnWithExitCode("rsync", ["-r", "-p", "--delete", "dist", process.env.SERVER_ROOT]);
 
-        rsync.addListener("exit", code => {
-            if (code !== 0)
-                throw new Error("`rsync' failed!");
-        })
-    });
+    if (rsync !== 0)
+        console.warn("[webhook] `rsync' failed!");
 };
 
 const webhookServer = createServer(handleWebhook);
